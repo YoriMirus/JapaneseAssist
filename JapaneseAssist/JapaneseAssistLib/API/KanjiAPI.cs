d39b082832e6ce567700dde9aba133973ab741c5
YoriMirus <misawalach00@seznam.cz>
2021-03-07 14:33:16 +0100

    Lookup kanji information on kanjiapi.dev and displaying it

---
+++ JapaneseAssist/JapaneseAssistLib/API/KanjiAPI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using JapaneseAssistLib.Models;
+
+namespace JapaneseAssistLib.API
+{
+    
+    public static class KanjiAPI
+    {
+        private static HttpClient Client;
+
+        public static async Task<KanjiAPIEntry> GetKanjiInfoAsync(char kanji)
+        {
+            using (Client = new HttpClient())
+            {
+                string json = await Task.Run(() => Client.GetStringAsync("https://kanjiapi.dev/v1/kanji/" + kanji));
+                KanjiAPIEntry entry = JsonSerializer.Deserialize<KanjiAPIEntry>(json);
+                return entry;
+            }
+        }
+    }
+}
